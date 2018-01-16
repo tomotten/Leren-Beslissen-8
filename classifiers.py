8d111639645a3258e248af015c054780d37eb783
donald <donald.nikkessen@gmail.com>
2018-01-16 14:40:01 +0100

    classifiers accuracy comparison'

--- classifiers.py
+++ classifiers.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import accuracy_score
 from sklearn.feature_selection import RFECV
 from sklearn import svm, linear_model
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 import re
 import csv
 from data import *
@@ -17,7 +20,7 @@ df = load_data('train.csv')
 x, y = split_X_R(df)
 x = remove_missing(x)
 
-print(x.describe())
+#print(x.describe())
 
 # Load in test data, and clean if needed
 test_df = load_data('test.csv')
@@ -25,11 +28,17 @@ test_df = remove_missing(test_df)
 
 # Fit model to training data
 
-# Xgboost Classifier
-xgbmodel = xgb.XGBClassifier()
+# Classifiers
+#xgbmodel = xgb.XGBClassifier()
 logistic = linear_model.LogisticRegression(C=1e5)
+svc = svm.SVC()
+knn = KNeighborsClassifier()
+rforest = RandomForestClassifier(max_depth=4, random_state=0)
 
-models = {"xgboost": xgbmodel, "logistic": logistic}
+models = {"logistic": logistic, \
+"svc": svc, "knn":knn,"rforest":rforest}
+result = []
+n = 0
 for key,model in models.items():
     print(key)
     model.fit(x.drop('PassengerId',1), y)
@@ -37,7 +46,7 @@ for key,model in models.items():
     scores = scores*100
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-res = output_pred(model, test_df.drop('PassengerId', 1), test_df['PassengerId'])
+res = output_pred(rforest, test_df.drop('PassengerId', 1), test_df['PassengerId'])
 filename = 'output.csv'
 res.to_csv(filename, index=False)
 
