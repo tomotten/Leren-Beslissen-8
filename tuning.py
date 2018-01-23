5fcbba42fce02a57c78065527dd9916ac9dea6b2
donald <donald.nikkessen@gmail.com>
2018-01-23 15:17:30 +0100

    removed gridcv

--- tuning.py
+++ tuning.py
@@ -1,7 +1,6 @@
 from sklearn.feature_selection import SelectFromModel
 import xgboost as xgb
 from sklearn.metrics import accuracy_score
-#from sklearn.grid_search import GridSearchCV   #Perforing grid search
 
 def fit_importance(model, x, y):
     # Fit model using each importance as a threshold
@@ -20,14 +19,5 @@ def fit_importance(model, x, y):
     	accuracy = accuracy_score(y, predictions)
     	print("Thresh=%.3f, n=%d, Accuracy: %.2f%%" % (thresh, select_X_train.shape[1], accuracy*100.0))
 
-def find_depth(x,y):
-    param_test1 = {
-     'max_depth':list(range(9,10,1)),
-     'min_child_weight':list(range(1,2,1)),
-     # 'n_estimators':list(range(10,100,10))
-    }
-    gsearch1 = GridSearchCV(estimator = xgb.XGBClassifier( learning_rate =0.1, gamma=0, subsample=0.8,
-     objective= 'binary:logistic', nthread=4, scale_pos_weight=1),
-     param_grid = param_test1, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
-    gsearch1.fit(x,y)
-    gsearch1.grid_scores_, gsearch1.best_params_, gsearch1.best_score_
+def find_depth():
+    
